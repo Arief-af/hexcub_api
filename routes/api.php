20834d82e2662e907b6c6fdcea8fd18118682fbf
Arief-Af <sekolah.arief@gmail.com>
2024-09-18 19:15:50 +0700

    fix(core): api init

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,23 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\MeetController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 // Contact CRUD Routes
-Route::get('contacts', [ContactController::class, 'index']);
-Route::post('contacts', [ContactController::class, 'store']);
-Route::get('contacts/{id}', [ContactController::class, 'show']);
-Route::put('contacts/{id}', [ContactController::class, 'update']);
-Route::delete('contacts/{id}', [ContactController::class, 'destroy']);
+Route::get('/contacts', [ContactController::class, 'index']);
+Route::post('/contacts', [ContactController::class, 'store']);
+Route::get('/contacts/{id}', [ContactController::class, 'show']);
+Route::put('/contacts/{id}', [ContactController::class, 'update']);
+Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
 
 // Meet CRUD Routes
-Route::get('meets', [MeetController::class, 'index']);
-Route::post('meets', [MeetController::class, 'store']);
-Route::get('meets/{id}', [MeetController::class, 'show']);
-Route::put('meets/{id}', [MeetController::class, 'update']);
-Route::delete('meets/{id}', [MeetController::class, 'destroy']);
+Route::get('/meets', [MeetController::class, 'index']);
+Route::post('/meets', [MeetController::class, 'store']);
+Route::get('/meets/{id}', [MeetController::class, 'show']);
+Route::put('/meets/{id}', [MeetController::class, 'update']);
+Route::delete('/meets/{id}', [MeetController::class, 'destroy']);
