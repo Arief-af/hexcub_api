6747885581a51263d435d31e6537a7a946c3153b
DavinArisandi <davinarisandi93@gmail.com>
2024-09-30 21:44:18 +0700

    fix(BaseController): fix sendResponse & sendError

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller as Controller;
 
 class BaseController extends Controller
@@ -10,35 +11,40 @@ class BaseController extends Controller
     /**
      * success response method.
      *
-     * @return \Illuminate\Http\Response
+     * @param $result
+     * @param $message
+     * @return JsonResponse
      */
-    public function sendResponse($result, $message)
+    public function sendResponse($result, $message): JsonResponse
     {
         $response = [
             'success' => true,
             'data'    => $result,
             'message' => $message,
         ];
-  
+
         return response()->json($response, 200);
     }
-  
+
     /**
      * return error response.
      *
-     * @return \Illuminate\Http\Response
+     * @param $error
+     * @param array $errorMessages
+     * @param int $code
+     * @return JsonResponse
      */
-    public function sendError($error, $errorMessages = [], $code = 404)
+    public function sendError($error, $errorMessages = [], $code = 404): JsonResponse
     {
         $response = [
             'success' => false,
             'message' => $error,
         ];
-  
-        if(!empty($errorMessages)){
+
+        if (!empty($errorMessages)) {
             $response['data'] = $errorMessages;
         }
-  
+
         return response()->json($response, $code);
     }
 }
