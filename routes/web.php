557f76e87f6637fb7712223dbb0c85cdc7531560
Arief Akhmad Fauzi <sekolah.arief@gmail.com>
2024-11-09 16:02:26 +0700

    dir

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,13 @@ Route::get('/', function () {
     $uploadMaxFilesize = ini_get('upload_max_filesize');
     $postMaxSize = ini_get('post_max_size');
     $phpVersion = phpversion();
+    $location = ini_get('extension_dir'); // fixed this line
     
     return response()->json([
         'php_version' => $phpVersion,
         'upload_max_filesize' => $uploadMaxFilesize,
         'post_max_size' => $postMaxSize,
+        'location' => $location // added location value
     ]);
 });
 
