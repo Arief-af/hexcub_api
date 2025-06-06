dd7333e5a10a51d90557280a97dd43003f5d81da
Arief-Af <sekolah.arief@gmail.com>
2025-06-06 22:53:08 +0700

    fix(core): CICD

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'Hi' => "Hayo mau ngapain kamu? HAHAHAHAHAH",
+        'version' => "1.0.0"
     ]);
 });
 
